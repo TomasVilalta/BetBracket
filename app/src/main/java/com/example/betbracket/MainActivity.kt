297b703433fa81b8d84b5f5f5c0f53a5175c44d5
TomasVilalta <tomasfedericovilalta@gmail.com>
2023-02-02 14:57:25 -0300

    Setup Appbars and Bottom navigation

--- app/src/main/java/com/example/betbracket/MainActivity.kt
+++ app/src/main/java/com/example/betbracket/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.betbracket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.eventsFragment, R.id.rankingsFragment,R.id.playersFragment))
+
+        val navController = navHostFragment.navController
+
+
+        setSupportActionBar(findViewById(R.id.topAppBar))
+        topAppBar.navigationIcon = null
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
